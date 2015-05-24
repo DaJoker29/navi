30d84ca72eae2fe63969040a5b6e996ea9978712
Dewitt Buckingham <dj729@comcast.net>
2015-05-23 19:03:17 -0700

    Fully functional module
    Created the basic functionality. Will add more things to this later.

--- src/js/Navi.js
+++ src/js/Navi.js
@@ -1,8 +1,65 @@
 var Navi = (function ( module ) {
 
-    // Initialize Module
-    module.init = function ( target, selector ) {
+    /* BUILD */
+    var build = function( args ) {
 
+        var container = document.querySelector(args.container);
+        var target = document.querySelectorAll(args.target);
+        // Create elements
+        var fragment = document.createDocumentFragment();
+        var menu = document.createElement('select');
+        
+        // Set element values
+        menu.id = menu.className = 'navi';
+        fragment.appendChild(menu);
+
+        // Create/Add default option
+        var option = document.createElement('option');
+        option.value = '';
+        option.disabled = true;
+        option.selected = true;
+        option.textContent = 'Navigation';
+        menu.appendChild(option);
+
+        // Populate menu with options
+        for(var i = 0; i < target.length; i++) {
+            // Build option
+            option = document.createElement('option');
+            option.value = '#' + target[i].id;
+            option.textContent = target[i].textContent;
+
+            // Add to menu
+            menu.appendChild(option);
+        }
+
+        // Add to DOM
+        container.appendChild(fragment);
+    };
+
+    /* LISTEN */
+    var listener = function() {
+        var el = document.querySelector('#navi');
+        el.addEventListener('change', function( e ) {
+            var selection = e.target.value;
+
+            if(selection === '') {
+                return;
+            }
+
+            if(typeof smoothScroll !== 'undefined') {
+                smoothScroll.animateScroll( null, selection );
+            } else {
+                selection = document.querySelector(selection);
+                selection.scrollIntoView();
+            }
+            e.target.selectedIndex = 0;
+        });
+    };
+
+    /* INITIALIZE */
+    module.init = function ( args ) {
+        build(args);
+        listener();
     };
 
     return module;
